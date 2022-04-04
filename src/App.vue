75c0765d02a27fc128b7effa86f70d5b00c5b53c
Olufunbi <funbybabalola@gmail.com>
2022-04-04 21:24:32 +0100

    feat: Create functions to retrun order state, added CSS to the home page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="app">
-    <JobList :jobs="jobs" />
+    <header>
+      <div class="order">
+        <button @click="handleClick('title')">Order by title</button>
+        <button @click="handleClick('salary')">Order by salary</button>
+        <button @click="handleClick('location')">Order by location</button>
+      </div>
+    </header>
+    
+    <JobList :jobs="jobs" :order="order" />
   </div>
 </template>
 
@@ -8,6 +16,7 @@
 import { defineComponent, reactive, ref, toRefs } from 'vue';
 import JobList from './components/JobsList.vue'
 import Job from './types/Job'
+import OrderTerm from './types/OrderTerm'
 
 export default defineComponent({
   name: 'App',
@@ -20,13 +29,33 @@ export default defineComponent({
       { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
       { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
     ])
+    const order = ref<OrderTerm>('title')
 
-    return { jobs } 
+    const handleClick = (term: OrderTerm) => {
+      order.value = term
+    }
+
+    return { jobs, handleClick, order } 
   },
   
 });
 </script>
 
 <style>
-
+header {
+    text-align: center;
+  }
+  header .order {
+    margin-top: 20px;
+  }
+  button {
+    margin: 0 10px;
+    color: #1195c9;
+    border: 3px solid #1195c9;
+    background: #d5f0ff;
+    padding: 8px 16px;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: bold;
+  }
 </style>
