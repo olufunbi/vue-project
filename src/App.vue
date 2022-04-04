038a4315f91098ec3676aaa8d199d63973a1ebab
Olufunbi <funbybabalola@gmail.com>
2022-04-04 21:51:40 +0100

    feat: Added more css and animations to the code base

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="app">
     <header>
+      <div class="title">
+        <img src="./assets/heart.svg" alt="site logo">
+        <h1>Hyrule Jobs</h1>
+      </div>
       <div class="order">
         <button @click="handleClick('title')">Order by title</button>
         <button @click="handleClick('salary')">Order by salary</button>
@@ -42,7 +46,7 @@ export default defineComponent({
 </script>
 
 <style>
-header {
+  header {
     text-align: center;
   }
   header .order {
@@ -57,5 +61,16 @@ header {
     border-radius: 4px;
     cursor: pointer;
     font-weight: bold;
+  }
+  header .title{
+    display: flex;
+    justify-content: center;
+  }
+  header img {
+    width: 60px;
+    margin-right: 20px;
+  }
+  header h1 {
+    font-size: 3em;
   }
 </style>
