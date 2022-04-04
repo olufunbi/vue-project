ca9a61f8afb991f4ef245171c19f5f711b1caa25
Olufunbi <funbybabalola@gmail.com>
2022-04-04 21:33:02 +0100

    feat: Added functions to loop through job and sort

--- src/components/JobsList.vue
+++ src/components/JobsList.vue
@@ -2,7 +2,7 @@
   <div class="job-list">
     <p>Ordered by {{ order }}</p>
     <ul>
-      <li v-for="job in jobs" :key="job.id">
+      <li v-for="job in orderedJobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
         <div class="salary">
           <p>{{ job.salary }} rupees</p>
@@ -21,7 +21,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
+import { computed, defineComponent, PropType } from "vue";
 import Job from "@/types/Job";
 import OrderTerm from "@/types/OrderTerm";
 
@@ -36,12 +36,21 @@ export default defineComponent({
       type: String as PropType<OrderTerm>
     },
   },
+  setup(props) {
+    const orderedJobs = computed(() => {
+      return [...props.jobs].sort((a: Job, b:Job) => {
+        return a[props.order] > b[props.order] ? 1 : -1
+      })
+    })
+
+    return { orderedJobs }
+  }
 });
 </script>
 
 <style scoped>
 .job-list {
-  max-width: 960px;
+  max-width: 960px; 
   margin: 40px auto;
 }
 .job-list ul {
